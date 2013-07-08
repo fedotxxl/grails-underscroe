c6483da0dc71c15cfc86848d45c1419a9fabc52b
fedor.belov <fedor.belov@mail.ru>
2013-07-08 17:18:35 +0400

    added don't cache method

--- src/groovy/io/belov/underscore/_web.groovy
+++ src/groovy/io/belov/underscore/_web.groovy
@@ -7,10 +7,15 @@ package io.belov.underscore
 import org.springframework.web.context.request.RequestContextHolder
 
 import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 class _web {
 
-    static {
+    private static final HEADER_PRAGMA = "Pragma";
+    private static final HEADER_EXPIRES = "Expires";
+    private static final HEADER_CACHE_CONTROL = "Cache-Control";
+
+    static void addIsAjaxMethod() {
         HttpServletRequest.metaClass.isAjax = {->
             'XMLHttpRequest' == delegate.getHeader('X-Requested-With')
         }
@@ -23,4 +28,12 @@ class _web {
         return RequestContextHolder.currentRequestAttributes().getRequest();
     }
 
+    static doNotCache(HttpServletResponse response) {
+        //http://stackoverflow.com/questions/49547/making-sure-a-web-page-is-not-cached-across-all-browsers
+        //http://groovyguts.wordpress.com/2010/02/15/preventing-caching-in-grails/
+        response.setHeader(HEADER_PRAGMA, "no-cache");
+        response.setDateHeader(HEADER_EXPIRES, 1L);
+        response.setHeader(HEADER_CACHE_CONTROL, "no-cache");
+        response.addHeader(HEADER_CACHE_CONTROL, "no-store");
+    }
 }
